059b4f6a5aa745c67b42df36ab3d4d55acff7f33
Max Ng <maxwindiff@gmail.com>
2021-12-07 00:10:37 -0800

    day7

---
+++ day07.jl
@@ -0,0 +1,20 @@
+nums = sort(map(x -> parse(Int, x), split(readline("day07a.txt"), ",")))
+
+#median = nums[length(nums) ÷ 2]
+#println(median, ": ", sum(abs.(nums .- median)))
+
+cost1(x) = x
+cost2(x) = x * (x+1) ÷ 2
+
+for cost_func ∈ [cost1, cost2]
+  min_pos = nothing
+  min_cost = typemax(Int)
+  for i ∈ nums[1]:nums[end]
+    cost = sum(cost_func.(abs.(nums .- i)))
+    if cost < min_cost
+      min_pos = i
+      min_cost = cost
+    end
+  end
+  println(min_pos, ": ", min_cost)
+end
