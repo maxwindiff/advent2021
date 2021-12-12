f33adedf8c914ac4afe891350abf8f37ddec87e6
Max Ng <maxwindiff@gmail.com>
2021-12-12 15:45:51 -0800

    simplification?

--- day12.jl
+++ day12.jl
@@ -3,21 +3,20 @@ using DataStructures: DefaultDict
 graph = DefaultDict{String, Vector{String}}([])
 for line ∈ eachline("data/day12.txt")
   a, b = split(line, "-")
-  push!(graph[a], b)
-  push!(graph[b], a)
+  b != "start" && push!(graph[a], b)
+  a != "start" && push!(graph[b], a)
 end
 
 function search(visited, loc, allow_one_revisit)
   if loc == "end"
     return 1
-  elseif loc[1] ∈ 'a':'z' && visited[loc] > 0
-    if !allow_one_revisit || any(>(1), (v for (k, v) ∈ visited if k[1] ∈ 'a':'z'))
-      return 0
-    end
+  elseif visited[loc] > 0 && !allow_one_revisit
+    return 0
   end
-  visited[loc] += 1
-  ret = sum(search(visited, next, allow_one_revisit) for next ∈ graph[loc] if next != "start")
-  visited[loc] -= 1
+
+  loc[1] ∈ 'a':'z' && (visited[loc] += 1)
+  ret = sum(search(visited, next, allow_one_revisit && visited[loc] < 2) for next ∈ graph[loc])
+  loc[1] ∈ 'a':'z' && (visited[loc] -= 1)
   return ret
 end
 
