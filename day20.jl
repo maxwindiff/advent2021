26ca46deead40bf11777d57a336d45f64d453578
Max Ng <maxwindiff@gmail.com>
2021-12-19 21:37:31 -0800

    day20

---
+++ day20.jl
@@ -0,0 +1,30 @@
+using BitBasis: packbits
+using OffsetArrays
+
+function enhance(img, mapping)
+  out = zero(img)
+  ax, ay = axes(img)
+  for i ∈ ax[2]:ax[end-1], j ∈ ay[2]:ay[end-1]
+    bits = reverse(vcat((img[x, j-1:j+1] for x ∈ (i-1):(i+1))...))
+    out[i, j] = mapping[packbits(bits)]
+  end
+  border = mapping[packbits(repeat([img[1,1]], 9))]
+  out[1, 1:end] = out[end, 1:end] = out[1:end, 1] = out[1:end, end] .= border
+  return out
+end
+
+lines = replace.(readlines("data/day20.txt"), '.' => '0', '#' => '1')
+
+mapping = OffsetArray(parse.(Int, split(lines[1], "")), 0:511)
+input = vcat(map(lines[3:end]) do line
+  transpose(parse.(Int, split(line, "")))
+end...)
+
+t = 50
+sx, sy = size(input)
+buf = zeros(Bool, sx+t*4, sy+t*4)
+buf[(1+t*2):(sx+t*2), (1+t*2):(sy+t*2)] = input
+for i ∈ 1:t
+  global buf = enhance(buf, mapping)
+  println("$i: $(sum(buf))")
+end
