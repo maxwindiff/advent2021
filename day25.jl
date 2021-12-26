973c931ce22e7a0087523426183f823079742ea2
Max Ng <maxwindiff@gmail.com>
2021-12-25 18:40:43 -0800

    refactor

--- day25.jl
+++ day25.jl
@@ -1,14 +1,10 @@
 using CircularArrays
 
-pp(a) = println(join(join.(eachrow(a), ""), "\n"), "\n")
-
-movements = ['>' => CartesianIndex(0, 1), 'v' => CartesianIndex(1, 0)]
-
 m = CircularArray(reduce(vcat, permutedims.(collect.(eachline("data/day25.txt")))))
 
 for i ∈ 1:1000
   moved = false
-  for (char, dir) ∈ movements
+  for (char, dir) ∈ ['>' => CartesianIndex(0, 1), 'v' => CartesianIndex(1, 0)]
     movers = [idx for idx ∈ findall(m .== char) if m[idx + dir] == '.']
     m[movers] .= '.'
     m[movers .+ [dir]] .= char
